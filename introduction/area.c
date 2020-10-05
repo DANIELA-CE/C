a1ef7adda93212738f5410339785081c93800a25
DANIELA CASTILLO <DANIELA-CE@users.noreply.github.com>
2020-10-05 15:36:28 -0500

    Area
    Explique funciones

--- introduction/area.c
+++ introduction/area.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
-#include<math.h>
+#include<stdio.h> //Permite entradas y salidas
+#include<math.h>	// Permite hacer cosas matemáticas
 #define PI 3.1416
 
-int main(){
-	float area, radio;
-	radio = 5;
-	area = PI * pow(5,2);
-	printf("Area\n");
-	printf("%s%f\n\n", "Area de Circulo con radio 5:", area);
-}
+int main(){	// Int espera variables de enteros
+	float area, radio;	//es una variante flotante
+	radio = 5;		//Valor del diario
+	area = PI * pow(5,2);	//pow  es potencia (base, potencia)
+	printf("Area\n");	// el \n es para salto de línea
+	printf("%s%f\n\n", "Area de Circulo con radio 5:", area);	//%F para imprimir varibla flotante
+	return 0; 	// al final para cuanadousamos int
+	}
